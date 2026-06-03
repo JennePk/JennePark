214487a129de47eaed34045ab959acffa9da0c59
JennePk <handmade.junglej@gmail.com>
2026-06-03 16:09:55 +0200

    Create tracking.js

---
+++ js/tracking.js
@@ -0,0 +1,42 @@
+window.dataLayer = window.dataLayer || [];
+
+function initArticleTracking() {
+  const likeBtn = document.getElementById("btn-like");
+  const shareX = document.getElementById("share-x");
+  const shareLinkedin = document.getElementById("share-linkedin");
+  const shareThreads = document.getElementById("share-threads");
+
+  if (likeBtn) {
+    likeBtn.addEventListener("click", likeArticle);
+  }
+
+  if (shareX) {
+    shareX.addEventListener("click", () => {
+      dataLayer.push({
+        event: "social_share",
+        network: "x",
+        page_path: window.location.pathname
+      });
+    });
+  }
+
+  if (shareLinkedin) {
+    shareLinkedin.addEventListener("click", () => {
+      dataLayer.push({
+        event: "social_share",
+        network: "linkedin",
+        page_path: window.location.pathname
+      });
+    });
+  }
+
+  if (shareThreads) {
+    shareThreads.addEventListener("click", () => {
+      dataLayer.push({
+        event: "social_share",
+        network: "threads",
+        page_path: window.location.pathname
+      });
+    });
+  }
+}
