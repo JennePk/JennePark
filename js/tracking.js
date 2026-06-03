8970310af5559d27de3600d9217dd49bc952c769
JennePk <handmade.junglej@gmail.com>
2026-06-03 18:45:49 +0200

    Update tracking.js

--- js/tracking.js
+++ js/tracking.js
@@ -1,10 +1,9 @@
 window.dataLayer = window.dataLayer || [];
 
-// LIKE EVENT
 function likeArticle() {
   alert("Thanks for liking ♡");
 
-  dataLayer.push({
+  window.dataLayer.push({
     event: "article_like",
     article_id: window.location.pathname.replace(".html", ""),
     article_title: document.title,
@@ -12,20 +11,20 @@ function likeArticle() {
   });
 }
 
-// INIT ALL TRACKING
 function initArticleTracking() {
   const likeBtn = document.getElementById("btn-like");
-  const shareX = document.getElementById("share-x");
-  const shareLinkedin = document.getElementById("share-linkedin");
-  const shareThreads = document.getElementById("share-threads");
 
   if (likeBtn) {
     likeBtn.addEventListener("click", likeArticle);
   }
 
+  const shareX = document.getElementById("share-x");
+  const shareLinkedin = document.getElementById("share-linkedin");
+  const shareThreads = document.getElementById("share-threads");
+
   if (shareX) {
     shareX.addEventListener("click", () => {
-      dataLayer.push({
+      window.dataLayer.push({
         event: "social_share",
         network: "x",
         page_path: window.location.pathname
@@ -35,7 +34,7 @@ function initArticleTracking() {
 
   if (shareLinkedin) {
     shareLinkedin.addEventListener("click", () => {
-      dataLayer.push({
+      window.dataLayer.push({
         event: "social_share",
         network: "linkedin",
         page_path: window.location.pathname
@@ -45,7 +44,7 @@ function initArticleTracking() {
 
   if (shareThreads) {
     shareThreads.addEventListener("click", () => {
-      dataLayer.push({
+      window.dataLayer.push({
         event: "social_share",
         network: "threads",
         page_path: window.location.pathname
